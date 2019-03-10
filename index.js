92af06937b8a0bdb574f10b3d5fd6dcfdf2b0c8d
vanished <ibrahimcajlakovic1@gmail.com>
2019-03-10 10:58:19 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var randomatic = require('randomatic');
+const uuid = require('uuid');
+
+/**
+ * Returns a random number.
+ * @return {Number}
+ */
+function randomNumber(len) {
+  return Math.random() * len;
+}
+
+/**
+ * Returns a random integer.
+ * @return {Number}
+ */
+function randomInteger(len) {
+  return Math.floor(randomNumber());
+}
+
+/**
+ * Returns a random boolean.
+ * @return {Boolean}
+ */
+function randomBoolean() {
+  return Boolean(Math.floor(Math.random() * 2));
+}
+
+/**
+ * Returns a random string.
+ * @return {String}
+ */
+function randomString(len) {
+  return randomatic('*', Math.floor(Math.random() * len));
+}
+
+/**
+ * Returns a random hash.
+ * @return {String}
+ */
+function randomHash(len) {
+  let val = uuid().replace(/\-/g, '');
+  
+  while (val.len < len) {
+    val += randomHash()
+  }
+  return val.substr(0, len)
+}
+
+console.log(randomHash(7));
+
+module.exports = {
+  randomNumber,
+  randomInteger,
+  randomBoolean,
+  randomString,
+  randomHash
+};
+
